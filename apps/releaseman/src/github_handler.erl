6c6c51bfc98958ad5916c0a4bc1314c919bbf80e
Maxim Sokhatsky <maxim@synrc.com>
2013-07-03 04:34:10 +0300

    allowed users

--- apps/releaseman/src/github_handler.erl
+++ apps/releaseman/src/github_handler.erl
@@ -9,12 +9,20 @@ handle(Req, State) ->
     {Params,NewReq} = cowboy_req:path(Req),
     Path = lists:reverse(string:tokens(binary_to_list(Params),"/")),
     [Repo,User|Rest] = Path,
-    case global:whereis_name("builder") of
-        undefined -> spawn(fun() -> global:register_name("builder",self()), builder() end);
-        Pid -> global:send("builder",{build,Repo,User}) end,
-    HTML = wf:to_binary(["<h1>202 Project Started to Build</h1><a href=\"/index?release=",User,"-",Repo,"\">",User,"-",Repo,"</a>"]),
-    {ok, Req3} = cowboy_req:reply(202, [], HTML, NewReq),
-    {ok, Req3, State}.
+    Allowed = lists:member(User,["5HT","doxtop","voxoz","synrc","spawnproc"]),
+    ResponseReq = case Allowed of
+        true ->
+            case global:whereis_name("builder") of
+                undefined -> spawn(fun() -> global:register_name("builder",self()), builder() end);
+                Pid -> global:send("builder",{build,Repo,User}) end,
+            HTML = wf:to_binary(["<h1>202 Project Started to Build</h1><a href=\"/index?release=",User,"-",Repo,"\">",User,"-",Repo,"</a>"]),
+            {ok, Req3} = cowboy_req:reply(202, [], HTML, NewReq),
+            Req3;
+        false ->
+            NA = wf:to_binary(["<h1>404 User not allowed</h1>"]),
+            {ok, Req4} = cowboy_req:reply(404, [], NA, NewReq),
+            Req4 end,
+    {ok, ResponseReq, State}.
 
 terminate(_Reason, _Req, _State) -> ok.
 
