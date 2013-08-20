a1ae7cef7df7f6c79663f18cae3a52f82756980e
Vladimir Kirillov <proger@wilab.org.ua>
2013-08-20 13:12:54 +0300

    sh safety

--- apps/builder/src/sh.erl
+++ apps/builder/src/sh.erl
@@ -1,15 +1,37 @@
 -module(sh).
--compile([export_all]).
+-export([run/1, run/2, run/3, run/4]).
 
+run(C) ->
+    run(C, binary, ".").
+
+run(C, Log) ->
+    run(C, Log, ".").
+
+run([C|Args], Log, Cwd) when is_list(C) ->
+    run(os:find_executable(C), Args, Log, Cwd);
 run(Command, Log, Cwd) when is_list(Command) ->
+    run("/bin/sh", ["-c", Command], Log, Cwd).
+
+run(Command, Args, binary, Cwd) ->
+    Self = self(),
+    spawn_link(fun() ->
+                Port = erlang:open_port({spawn_executable, Command},
+                    [stream, stderr_to_stdout, binary, exit_status,
+                        {args, Args}, {cd, Cwd}]),
+
+                error_logger:info_msg("~p: ~p", [Port, erlang:port_info(Port)]),
+                handle_loop(Self, binary, [])
+        end),
+    wait();
+run(Command, Args, Log, Cwd) ->
     Self = self(),
     spawn_link(fun() ->
                 {ok, File} = file:open(Log, [append, raw]),
-                file:write(File, [">>>> ", Command, "\n"]),
+                file:write(File, [">>>> ", Command, " ", [[A, " "] || A <- Args], "\n"]),
 
-                Port = erlang:open_port({spawn_executable, "/bin/sh"},
+                Port = erlang:open_port({spawn_executable, Command},
                     [stream, stderr_to_stdout, binary, exit_status,
-                        {args, ["-c", Command]}, {cd, Cwd}]),
+                        {args, Args}, {cd, Cwd}]),
 
                 error_logger:info_msg("~p: ~p", [Port, erlang:port_info(Port)]),
                 handle_loop(Self, File)
@@ -17,7 +39,17 @@ run(Command, Log, Cwd) when is_list(Command) ->
     wait().
 
 wait() ->
-    receive done -> ok end.
+    receive {done, Status, Data} -> {ok, Status, Data} end.
+
+handle_loop(Parent, binary, Acc) ->
+    receive
+        {_Port, {data, Data}} ->
+            handle_loop(Parent, binary, [Acc, Data]);
+        {_Port, {exit_status, Status}} ->
+            Parent ! {done, Status, iolist_to_binary(Acc)};
+        _M -> % discard
+            handle_loop(Parent, binary, Acc)
+    end.
 
 handle_loop(Parent, File) ->
     receive
@@ -27,9 +59,8 @@ handle_loop(Parent, File) ->
         {_Port, {exit_status, Status}} ->
             file:write(File, [">>>> exit status: ", integer_to_list(Status), "\n"]),
             file:close(File),
-            Parent ! done;
-        _M ->
-            % discard
+            Parent ! {done, Status, undefined};
+        _M -> % discard
             handle_loop(Parent, File)
     end.
 
